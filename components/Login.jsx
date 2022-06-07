464e1cbb698f1a53abee7484a62cd51f43fec254
muratozdemir <muratozdemir@siu.com.tr>
2022-06-07 22:03:22 +0300

    Initial commit

---
+++ components/Login.jsx
@@ -0,0 +1,13 @@
+const Login = () => {
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+        quaerat.
+      </p>
+    </div>
+  )
+}
+
+export default Login
