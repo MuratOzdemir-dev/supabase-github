681676c11da1fd210149208796e9efffafc90873
muratozdemir <muratozdemir@siu.com.tr>
2022-06-07 23:53:44 +0300

    components remove

--- components/Details.jsx
+++
@@ -1,7 +0,0 @@
-import { Button } from '@mantine/core'
-
-const Details = ({ getSession, session }) => {
-  return <div></div>
-}
-
-export default Details
--- components/Login.jsx
+++
@@ -1,7 +0,0 @@
-import { Button, Center, Grid, Group, Stack } from '@mantine/core'
-
-const Login = ({ loginHandler }) => {
-  return <Group align="center" justify="center"></Group>
-}
-
-export default Login
